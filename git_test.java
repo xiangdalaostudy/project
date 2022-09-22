92bbcdfd232cb3f4da244176f2852ab35d770874
lizq <652734148@qq.com>
2022-09-22 10:06:12 +0800

    push

--- git_test.java
+++ git_test.java
@@ -4,6 +4,9 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("你好---------git");
         System.out.println("你好---------git");
+        System.out.println("你好---------git");
+        System.out.println("你好---------git");
+        System.out.println("push---------git");
 
     }
 }
