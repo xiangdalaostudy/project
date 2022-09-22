7e7845d094b5e6b9232a650cc56e05f4b746b321
lizq <652734148@qq.com>
2022-09-22 09:18:55 +0800

    第二次提交

--- git_test.java
+++ git_test.java
@@ -3,5 +3,7 @@ package git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("你好---------git");
+        System.out.println("你好---------git");
+
     }
 }
